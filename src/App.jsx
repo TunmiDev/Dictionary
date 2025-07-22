e624e5b02952367dbc56a2d37aa4d07923203d57
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-07-22 04:33:27 +0100

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Dictionary from "../src/Dictionary";
 
 const App = () => {
-  return <div>hiiii</div>;
+  return (
+    <div>
+      <Dictionary />
+    </div>
+  );
 };
 
 export default App;
