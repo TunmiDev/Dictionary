a2b9a022ab9103530d085b2586fd2787986134ae
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-08-06 02:18:44 +0100

    added api

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -3,10 +3,16 @@ import axios from "axios";
 import "./Dictionary.css";
 const Dictionary = () => {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition...`);
+    let apiKey = "41b39faeco43443c5c35d963td510b86";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
