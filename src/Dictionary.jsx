142540fe41a28b093d6f005294ac9b7e9aa46c2f
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-08-07 03:23:00 +0100

    ..

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Result from "./Result";
+import Results from "./Results";
 import "./Dictionary.css";
-const Dictionary = () => {
+
+export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+  let [results, setResults] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiKey = "41b39faeco43443c5c35d963td510b86";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -26,9 +28,7 @@ const Dictionary = () => {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
-      <Result results={results} />
+      <Results results={results} />
     </div>
   );
-};
-
-export default Dictionary;
+}
