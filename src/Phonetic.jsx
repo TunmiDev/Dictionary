04ca063f75bff883852ee8e757573410b8248bd4
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-08-12 11:15:38 +0100

    ..

--- src/Phonetic.jsx
+++ src/Phonetic.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./Phonetic.css";
+import { FaVolumeUp } from "react-icons/fa"; // speaker icon from react-icons
 
 const Phonetic = (props) => {
-  console.log(props.phonetic);
+  function playAudio() {
+    if (props.phonetic.audio) {
+      const audio = new Audio(props.phonetic.audio);
+      audio.play();
+    }
+  }
+
   return (
     <div className="phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Listen
-      </a>
-      <span className="text"> {props.phonetic.text} </span>
+      <button
+        onClick={playAudio}
+        className="listen-button"
+        aria-label={`Play pronunciation for ${props.phonetic.text}`}
+      >
+        <FaVolumeUp size={20} /> {/* Speaker icon */}
+      </button>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 };
