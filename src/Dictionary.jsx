b48ab077a432e67579b5214fe2662882c93ef55c
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-08-12 11:05:57 +0100

    ..

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -7,15 +7,25 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState([]);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePixelResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
-    // documentation: https://dictionaryapi.dev/e
+    // Dictionary API
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // SheCodes Images API
+    let pixelsApiKey = "41b39faeco43443c5c35d963td510b86";
+    let pixelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pixelsApiKey}`;
+    axios.get(pixelsApiUrl).then(handlePixelResponse);
   }
 
   function handleSubmit(event) {
@@ -45,6 +55,11 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <section className="photos">
+          {photos.map((photo, index) => (
+            <img key={index} src={photo.src.landscape} alt={keyword} />
+          ))}
+        </section>
       </div>
     );
   } else {
